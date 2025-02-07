46304a3eb167e32adf4e7c44bee43d01148d08f6
kapuluta2 <167614820+kapuluta1@users.noreply.github.com>
2025-02-07 13:07:11 +0300

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "",
+SESSION_ID: process.env.SESSION_ID || " GMAX-MD~7MwkQLzT#SE2YDpwDuO4vv8teujyezzlBnMbxsqhePQzBWwsyrrg   ",
 // add your Session Id 
 AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN || "true",
 // make true or false status auto seen
@@ -13,9 +13,9 @@ AUTO_STATUS_REPLY: process.env.AUTO_STATUS_REPLY || "false",
 // make true if you want auto reply on status 
 AUTO_STATUS_MSG: process.env.AUTO_STATUS_MSG || "*SEEN YOUR STATUS BY GMAX🤍*",
 // set the auto reply massage on status reply  
-PREFIX: process.env.PREFIX || ",",
+PREFIX: process.env.PREFIX || ".",
 // add your prifix for bot   
-BOT_NAME: process.env.BOT_NAME || "GMAX-MD",
+BOT_NAME: process.env.BOT_NAME || "emeritus",
 // add bot namw here for menu
 STICKER_NAME: process.env.STICKER_NAME || "GMAX-MD",
 // type sticker pack name 
@@ -25,7 +25,7 @@ CUSTOM_REACT_EMOJIS: process.env.CUSTOM_REACT_EMOJIS || "💝,💖,💗,❤️�
 // chose custom react emojis by yourself 
 DELETE_LINKS: process.env.DELETE_LINKS || "false",
 // automatic delete links witho remove member 
-OWNER_NUMBER: process.env.OWNER_NUMBER || "255753853473",
+OWNER_NUMBER: process.env.OWNER_NUMBER || "255674477908",
 // add your bot owner number
 OWNER_NAME: process.env.OWNER_NAME || "Mr Gmax",
 // add bot owner name
@@ -51,7 +51,7 @@ AUTO_REPLY: process.env.AUTO_REPLY || "false",
 // make true or false automatic text reply 
 ALWAYS_ONLINE: process.env.ALWAYS_ONLINE || "false",
 // maks true for always online 
-PUBLIC_MODE: process.env.PUBLIC_MODE || "true",
+PUBLIC_MODE: process.env.PUBLIC_MODE || "false",
 // make false if want private mod
 AUTO_TYPING: process.env.AUTO_TYPING || "false",
 // true for automatic show typing   
